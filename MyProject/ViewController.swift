465186705e8c3fba9ba5e840180424c06db04631
lufengfeng <992128417@qq.com>
2017-10-23 19:01:50 +0800

    我的项目模板

--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+//import Cocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var stringA = "菜鸟教程："
+        stringA += "http://www.runoob.com"
+        print( stringA )
+        
+        // stringB 不能修改
+        let stringB = String("菜鸟教程：")
+       // stringB += "http://www.runoob.com"
+        print( stringB )
         // Do any additional setup after loading the view, typically from a nib.
     }
 
